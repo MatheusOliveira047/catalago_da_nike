66c3fb4015578817be9e79bf11f4e0836c76bf6c
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-04-18 20:26:42 -0300

    catalago da nike

---
+++ script.js
@@ -0,0 +1,120 @@
+//VAPORMAX
+let max = document.querySelector("#max")
+let max1 = document.querySelector("#max1")
+let max2 = document.querySelector("#max2")
+let max3 = document.querySelector("#max3")
+let max4 = document.querySelector("#max4")
+max1.addEventListener('click',()=>{
+    max.src = 'img/air-vapormax/tenis1.webp'
+})
+max2.addEventListener('click',()=>{
+    max.src = 'img/air-vapormax/vapormax2.webp'
+})
+max3.addEventListener('click',()=>{
+    max.src = 'img/air-vapormax/vapormax3.webp'
+})
+max4.addEventListener('click',()=>{
+    max.src = 'img/air-vapormax/vapormax4.webp'
+})
+
+
+//AIRMAX
+let airmax = document.querySelector("#airMax")
+let airmax1 = document.querySelector("#airMax1")
+let airmax2 = document.querySelector("#airMax2")
+let airmax3 = document.querySelector("#airMax3")
+let airmax4 = document.querySelector("#airMax4")
+airmax1.addEventListener('click',()=>{
+    airmax.src = 'img/AIR-MAX91/AIR_MAX_1.webp'
+})
+airmax2.addEventListener('click',()=>{
+    airmax.src = 'img/AIR-MAX91/AIR_MAX_2.webp'
+})
+airmax3.addEventListener('click',()=>{
+    airmax.src = 'img/AIR-MAX91/AIR_MAX_3.webp'
+})
+airmax4.addEventListener('click',()=>{
+    airmax.src = 'img/AIR-MAX91/AIR_MAX_4.webp'
+})
+
+
+//VORTEX
+let vortex = document.querySelector("#vortex")
+let vortex1 = document.querySelector("#vortex1")
+let vortex2 = document.querySelector("#vortex2")
+let vortex3 = document.querySelector("#vortex3")
+let vortex4 = document.querySelector("#vortex4")
+vortex1.addEventListener('click',()=>{
+    vortex.src = 'img/air-vortex/ari-vortex1.webp'
+})
+vortex2.addEventListener('click',()=>{
+    vortex.src = 'img/air-vortex/air-vortex2.png'
+})
+vortex3.addEventListener('click',()=>{
+    vortex.src = 'img/air-vortex/air-vortex3.webp'
+})
+vortex4.addEventListener('click',()=>{
+    vortex.src = 'img/air-vortex/air-vortex4.webp'
+})
+
+
+//AIRMAX270
+
+let air_max_270 = document.querySelector("#air_max_270")
+let air_max_270_1 = document.querySelector("#air_max_270_1")
+let air_max_270_2 = document.querySelector("#air_max_270_2")
+let air_max_270_3 = document.querySelector("#air_max_270_3")
+let air_max_270_4 = document.querySelector("#air_max_270_4")
+air_max_270_1.addEventListener('click',()=>{
+    air_max_270.src = 'img/AIR-MAX270/air-max-270-1.webp'
+})
+air_max_270_2.addEventListener('click',()=>{
+    air_max_270.src = 'img/AIR-MAX270/air-max-270-2.webp'
+})
+air_max_270_3.addEventListener('click',()=>{
+    air_max_270.src = 'img/AIR-MAX270/air-max-270-3.webp'
+})
+air_max_270_4.addEventListener('click',()=>{
+    air_max_270.src = 'img/AIR-MAX270/air-max-270-4.webp'
+})
+
+//
+
+let air_max_2090 = document.querySelector("#air_max_2090")
+let air_max_2090_1 = document.querySelector("#air_max_2090_1")
+let air_max_2090_2 = document.querySelector("#air_max_2090_2")
+let air_max_2090_3 = document.querySelector("#air_max_2090_3")
+let air_max_2090_4 = document.querySelector("#air_max_2090_4")
+
+air_max_2090_1.addEventListener('click',()=>{
+    air_max_2090.src = 'img/air-max-2090/tenis6.webp'
+})
+air_max_2090_2.addEventListener('click',()=>{
+    air_max_2090.src = 'img/air-max-2090/air_max_2090_2.webp'
+})
+air_max_2090_3.addEventListener('click',()=>{
+    air_max_2090.src = 'img/air-max-2090/air_max_2090_3.png'
+})
+air_max_2090_4.addEventListener('click',()=>{
+    air_max_2090.src = 'img/air-max-2090/air_max_2090_4.webp'
+})
+
+
+// 
+let air_max_inf = document.querySelector('#air_max_inf')
+let air_max_inf1 = document.querySelector('#air_max_inf1')
+let air_max_inf2 = document.querySelector('#air_max_inf2')
+let air_max_inf3 = document.querySelector('#air_max_inf3')
+let air_max_inf4 = document.querySelector('#air_max_inf4')
+air_max_inf1.addEventListener('click',()=>{
+    air_max_inf.src = 'img/air_max_infu/air_max_inf1.webp'
+})
+air_max_inf2.addEventListener('click',()=>{
+    air_max_inf.src = 'img/air_max_infu/air_max_inf2.webp'
+})
+air_max_inf3.addEventListener('click',()=>{
+    air_max_inf.src = 'img/air_max_infu/air_max_inf3.webp'
+})
+air_max_inf4.addEventListener('click',()=>{
+    air_max_inf.src = 'img/air_max_infu/air_max_inf4.webp'
+})
